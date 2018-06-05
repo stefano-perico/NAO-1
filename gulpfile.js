514459a298e95f37a344f453e5ae7ccf5beeb0cb
Fildus <davidgastaldello@msn.com>
2018-06-05 23:54:38 +0200

    Gulp, installation composants symfony

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let concat = require('gulp-concat');
+let minify = require('gulp-minify');
+let cleanCSS = require('gulp-clean-css');
+let uglifycss = require('gulp-uglifycss');
+let plumber = require('gulp-plumber');
+
+gulp.task('sass', function () {
+    return gulp.src('./assets/css/app.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({compatibility: 'edge'}))
+        .pipe(uglifycss({
+            "maxLineLen": 80,
+            "uglyComments": true
+        }))
+        .pipe(gulp.dest('./public/build/css'));
+});
+
+gulp.task('scriptsCompact', function() {
+    return gulp.src('./assets/js/compact/**/*')
+        .pipe(plumber())
+        .pipe(concat('compact.js'))
+        .pipe(minify())
+        .pipe(gulp.dest('./public/build/js/compact'));
+});
+
+gulp.task('scriptsScattered', function() {
+    return gulp.src('./assets/js/scattered/**/*')
+        .pipe(plumber())
+        .pipe(minify())
+        .pipe(gulp.dest('./public/build/js/scattered'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./assets/**/*', ['sass','scriptsCompact','scriptsScattered']);
+});
